b2cce55a9814c908b21553d343ec943dcb114dd2
Konrad Paluszek <kezsulap@gmail.com>
2025-12-26 13:39:39 +0100

    Don't compile .md files in make_all_pdf if they're up to date

--- make_all_pdf.sh
+++ make_all_pdf.sh
@@ -1,6 +1,13 @@
 mkdir pdf-rules -p
 set -e
 for x in $(ls rules | grep -v 'list_all.txt'); do 
-	pandoc rules/${x} -o pdf-rules/${x%md}pdf   --pdf-engine=xelatex   -V mainfont="DejaVu Serif" &
+	input_file=rules/${x}
+	output_file=pdf-rules/${x%md}pdf
+	if [ "$input_file" -ot "$output_file" ]; then
+		echo "$output_file is up to date";
+	else
+		echo -n "Compiling $output_file ... "
+		pandoc rules/${x} -o pdf-rules/${x%md}pdf   --pdf-engine=xelatex   -V mainfont="DejaVu Serif";
+		echo " Done";
+	fi
 done
-wait
