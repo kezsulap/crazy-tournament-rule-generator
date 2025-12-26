24b425f575457fbe7913bb8481198df206e1c68d
Konrad Paluszek <kezsulap@gmail.com>
2025-12-26 13:36:00 +0100

    Remove list_all.txt from files parsed in make_all_pdf.sh

--- make_all_pdf.sh
+++ make_all_pdf.sh
@@ -1,6 +1,6 @@
 mkdir pdf-rules -p
 set -e
-for x in $(ls rules); do 
+for x in $(ls rules | grep -v 'list_all.txt'); do 
 	pandoc rules/${x} -o pdf-rules/${x%md}pdf   --pdf-engine=xelatex   -V mainfont="DejaVu Serif" &
 done
 wait
